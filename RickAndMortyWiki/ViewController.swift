e5d374adbacf9629315c0c0d5312faca9b55546e
Miroslav Berezovsky <burningbear1337@gmail.com>
2022-12-31 09:09:44 +0700

    Initial pull

---
+++ RickAndMortyWiki/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  RickAndMortyWiki
+//
+//  Created by Miroslav Berezovsky on 31.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		view.backgroundColor = .green
+	}
+}
+
