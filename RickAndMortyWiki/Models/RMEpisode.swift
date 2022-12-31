19782e10103fb8323556f6c43253eb120a977cf0
Miroslav Berezovsky <burningbear1337@gmail.com>
2022-12-31 19:11:03 +0700

    [UPDATE]

--- RickAndMortyWiki/Models/RMEpisode.swift
+++ RickAndMortyWiki/Models/RMEpisode.swift
@@ -1,18 +1,17 @@
 //
-//  RMLocation.swift
+//  RMEpisode.swift
 //  RickAndMortyWiki
 //
 //  Created by Miroslav Berezovsky on 31.12.2022.
 //
 
-struct RMLocation: Codable
+struct RMEpisode: Codable
 {
 	let id: Int
 	let name: String
-	let type: String
-	let dimension: String
-	let residents: [String]
+	let air_date: String
+	let episode: String
+	let characters: [String]
 	let url: String
 	let created: String
 }
-
