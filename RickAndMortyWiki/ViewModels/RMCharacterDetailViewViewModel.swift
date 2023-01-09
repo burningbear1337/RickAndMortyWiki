bfcdcbf83f592b59f91539cd68479bbdaff81587
Miroslav Berezovsky <burningbear1337@gmail.com>
2023-01-09 14:26:22 +0700

    [Update]

--- RickAndMortyWiki/ViewModels/RMCharacterDetailViewViewModel.swift
+++ RickAndMortyWiki/ViewModels/RMCharacterDetailViewViewModel.swift
@@ -5,11 +5,20 @@
 //  Created by Miroslav Berezovsky on 03.01.2023.
 //
 
-import Foundation
+import UIKit
 
-final class RMCharacterDetailViewViewModel
+final class RMCharacterDetailViewViewModel: NSObject
 {
 	private let character: RMCharacter
+	
+	public enum SectionType: CaseIterable
+	{
+		case photo
+		case inforamtion
+		case episodes
+	}
+	
+	public let sections = SectionType.allCases
 	
 	public var title: String {
 		character.name.uppercased()
@@ -21,6 +30,139 @@ final class RMCharacterDetailViewViewModel
 	
 	init(character: RMCharacter) {
 		self.character = character
-		dump(character)
 	}
+}
+
+extension RMCharacterDetailViewViewModel: UICollectionViewDelegate,
+										  UICollectionViewDataSource
+{
+	func numberOfSections(in collectionView: UICollectionView) -> Int {
+		sections.count
+	}
+	
+	func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+		switch section {
+		case 0:
+			return 1
+		case 1:
+			return 2
+		case 2:
+			return 3
+		default:
+			return 0
+		}
+	}
+	
+	func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+		let cell = collectionView.dequeueReusableCell(
+			withReuseIdentifier: "cell",
+			for: indexPath
+		)
+		switch indexPath.section {
+		case 0:
+			cell.backgroundColor = .systemPink
+		case 1:
+			cell.backgroundColor = .yellow
+		case 2:
+			cell.backgroundColor = .red
+		default:
+			break
+		}
+		return cell
+	}
+}
+
+extension RMCharacterDetailViewViewModel
+{
+	func makeCollectionView() -> UICollectionView {
+		let layout = UICollectionViewCompositionalLayout { sectionIndex, _ in
+			return self.makeSection(for: sectionIndex)
+		}
+		let collectionView = UICollectionView(frame: .zero,
+											  collectionViewLayout: layout)
+		collectionView.register(UICollectionViewCell.self,
+								forCellWithReuseIdentifier: "cell")
+		collectionView.delegate = self
+		collectionView.dataSource = self
+		collectionView.translatesAutoresizingMaskIntoConstraints = false
+		return collectionView
+	}
+	
+	func makeSection(for index: Int) -> NSCollectionLayoutSection {
+		switch sections[index] {
+		case .photo:
+			return makePhotoSection()
+		case .inforamtion:
+			return makeInforamtionSection()
+		case .episodes:
+			return makeEpisodesSection()
+		}
+	}
+	
+	func makePhotoSection() -> NSCollectionLayoutSection {
+		let item = NSCollectionLayoutItem(
+			layoutSize: NSCollectionLayoutSize(
+				widthDimension: .fractionalWidth(1),
+				heightDimension: .fractionalHeight(1)
+			)
+		)
+		
+		item.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
+
+		let group = NSCollectionLayoutGroup.vertical(
+			layoutSize: NSCollectionLayoutSize(
+				widthDimension: .fractionalWidth(1),
+				heightDimension: .absolute(150)
+			),
+			subitems: [item]
+		)
+		
+		let section = NSCollectionLayoutSection(group: group)
+		return section
+	}
+	
+	func makeInforamtionSection() -> NSCollectionLayoutSection {
+		let item = NSCollectionLayoutItem(
+			layoutSize: NSCollectionLayoutSize(
+				widthDimension: .fractionalWidth(1),
+				heightDimension: .fractionalHeight(1)
+			)
+		)
+		
+		item.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
+
+		let group = NSCollectionLayoutGroup.vertical(
+			layoutSize: NSCollectionLayoutSize(
+				widthDimension: .fractionalWidth(1),
+				heightDimension: .absolute(150)
+			),
+			subitems: [item]
+		)
+		
+		let section = NSCollectionLayoutSection(group: group)
+		return section
+	}
+	
+	func makeEpisodesSection() -> NSCollectionLayoutSection {
+		let item = NSCollectionLayoutItem(
+			layoutSize: NSCollectionLayoutSize(
+				widthDimension: .fractionalWidth(1),
+				heightDimension: .fractionalHeight(1)
+			)
+		)
+		
+		item.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
+
+		let group = NSCollectionLayoutGroup.vertical(
+			layoutSize: NSCollectionLayoutSize(
+				widthDimension: .fractionalWidth(1),
+				heightDimension: .absolute(150)
+			),
+			subitems: [item]
+		)
+		
+		let section = NSCollectionLayoutSection(group: group)
+		return section
+	}
+
 }
--- RickAndMortyWiki/Views/RMCharacterDetailView.swift
+++ RickAndMortyWiki/Views/RMCharacterDetailView.swift
@@ -9,7 +9,7 @@ import UIKit
 
 final class RMCharacterDetailView: UIView
 {
-	private let characterViewModel: RMCharacterDetailViewViewModel
+	private let viewModel: RMCharacterDetailViewViewModel
 	
 //	private var collectionView: UICollectionView?
 	
@@ -21,10 +21,11 @@ final class RMCharacterDetailView: UIView
 	}()
 	
 	init(characterViewModel: RMCharacterDetailViewViewModel) {
-		self.characterViewModel = characterViewModel
+		self.viewModel = characterViewModel
 		super.init(frame: .zero)
 		backgroundColor = .systemBackground
 		setupView()
+		
 	}
 	
 	required init?(coder: NSCoder) {
@@ -35,44 +36,13 @@ final class RMCharacterDetailView: UIView
 private extension RMCharacterDetailView
 {
 	func setupView() {
-		let collectionView = makeCollectionView()
+		let collectionView = viewModel.makeCollectionView()
 		addSubviews(collectionView, spinner)
-		collectionView.backgroundColor = .yellow
 		NSLayoutConstraint.activate([
 			collectionView.leadingAnchor.constraint(equalTo: leadingAnchor),
 			collectionView.trailingAnchor.constraint(equalTo: trailingAnchor),
 			collectionView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
 			collectionView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor),
 		])
-	}
-	
-	func makeCollectionView() -> UICollectionView {
-		let layout = UICollectionViewCompositionalLayout { sectionIndex, _ in
-			return self.makeSection(for: sectionIndex)
-		}
-		let collectionView = UICollectionView(frame: .zero,
-											  collectionViewLayout: layout)
-		collectionView.register(UICollectionViewCell.self,
-								forCellWithReuseIdentifier: "cell")
-		collectionView.translatesAutoresizingMaskIntoConstraints = false
-		return collectionView
-	}
-	
-	func makeSection(for index: Int) -> NSCollectionLayoutSection {
-		let item = NSCollectionLayoutItem(
-			layoutSize: NSCollectionLayoutSize(
-				widthDimension: .fractionalWidth(1),
-				heightDimension: .fractionalHeight(1)
-			)
-		)
-		let group = NSCollectionLayoutGroup.vertical(
-			layoutSize: NSCollectionLayoutSize(
-				widthDimension: .absolute(150),
-				heightDimension: .absolute(150)
-			),
-			subitems: [item]
-		)
-		let section = NSCollectionLayoutSection(group: group)
-		return section
 	}
 }
